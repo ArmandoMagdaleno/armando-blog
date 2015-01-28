b081b8867cb1ea3c52600c7da79d9d62e181290e
Armando Magdaleno <magdalenoarmando90@gmail.com>
2015-01-28 11:28:31 -0800

    video 10

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,2 +1,13 @@
 <?php
-    require-once();
+    require_once(__DIR__ . "../model/database.php");
+    
+    $connection = new mysqli($host, $username, $password);
+    
+    if($connection->connect_error) {
+        die("Error: " . $connection->connect_error);
+    }
+    else {
+        echo "Success: " . $connection->host_info;
+    }
+    
+    $connection->close();
