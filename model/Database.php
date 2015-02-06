70719afc1f2820089a2a81bbc896af231e1c1932
Armando Magdaleno <magdalenoarmando90@gmail.com>
2015-02-06 11:24:28 -0800

    35-36

--- model/Database.php
+++ model/Database.php
@@ -5,14 +5,33 @@ class Database {
     private $username;
     private $password;
     private $database;
+    public $error;
     
     public function __construct($host, $username, $password, $database) {
         $this->host = $host;
         $this->username = $username;
         $this->password = $password;
         $this->database = $database;
+
+        $this->connection = new mysqli($host, $username, $password);
+
+        if ($this->connection->connect_error) {
+            die("<p> Error: " . $this->connection->connect_error . "</p>");
+        }
+
+        $exists = $this->connection->select_db($database);
+
+        if (!$exists) {
+            $query = $this->connection->query("CREATE DATABASE $database");
+
+            if ($query) {
+                echo "<p> Successfully created database " . $database . "</p>";
+            }
+        } else {
+            echo "<p> Database already exists </p>";
+        }
     }
-    
+
     public function openConnection() {
         $this->connection = new mysqli($this->host, $this->username, $this->password, $this->database);
 
@@ -31,6 +50,10 @@ class Database {
         $this->openConnection();
         
         $query = $this->connection->query($string);
+        
+        if(!$query) {
+            $this->error = $this->connection->error;
+        }
         
         $this->closeConnection();
         
